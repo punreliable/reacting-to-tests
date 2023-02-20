60295f776bc09d8e8fdf3e8806608b1aeb416433
Smith <BSmith9@cg9p9d0gyd.mi.corp.rockfin.com>
2023-02-20 18:01:09 -0500

    First successful tests written

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,3 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import Greeting from './Greeting';
+
 test( 'renders Hello World message', () => {
+
+render(<Greeting />);
+screen.getByText('Hello World!', {exact: true});
+const helloWorldElement = screen.getByText('Hello World', { exact: false});
+expect(helloWorldElement).toBeInTheDocument();
 
 });
